7254ee2737dc7a18ca4ae0d3ebd483ef2a04ad07
Ben Zellers <zellersdenver@gmail.com>
2023-05-23 12:46:36 -0400

    Exercise one complete

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double _balance;
+
+        public void Deposit(double moneyToAdd) 
+        {
+            Console.WriteLine($"Deposit of {moneyToAdd, 0:c} to your account");
+            _balance += moneyToAdd;   
+        }
+
+        public double GetBalance() 
+        {
+            return _balance;   
+        }
+    }
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BankAccount account = new BankAccount();
+
+            Console.WriteLine("How much money would you like to deposit today?");
+            double amountToDeposit = double.Parse(Console.ReadLine()); 
+            account.Deposit(amountToDeposit);
+            double userBalance = account.GetBalance();
+            
+
+            Console.WriteLine($"Your account balance is {userBalance,0:c}");
+            
         }
     }
 }
